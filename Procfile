412157e783a5b97cfc026bed0cd2b1ab0041779f
MANAS <manasrathore2342@gmail.com>
2022-03-18 17:03:51 +0530

    api

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-heroku ps:scale web=1
 web: gunicorn university_app.wsgi --log-file -
+heroku ps:scale web=1
--- university_app/settings.py
+++ university_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+#  "whitenoise.middleware.WhiteNoiseMiddleware"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
