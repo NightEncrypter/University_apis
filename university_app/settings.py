64346bb4a9547be9f797e1aaaef0777646ea4b90
MANAS <manasrathore2342@gmail.com>
2022-03-18 23:51:15 +0530

    try-1

--- university_app/settings.py
+++ university_app/settings.py
@@ -153,9 +153,11 @@ USE_TZ = True
 
 # serve file 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    
+]
 # combininig to all static files into staticfiles folder
-STATIC_ROOT=os.path.join(BASE_DIR,'static/')
+STATIC_ROOT=BASE_DIR /'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -163,9 +165,9 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT=BASE_DIR / 'media'
 
-MEDIA_URL='/media/'
+MEDIA_URL='media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
