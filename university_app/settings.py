bd105fae55c0afe48af965c0f9936e23bfc1ee44
MANAS <manasrathore2342@gmail.com>
2022-03-18 17:27:14 +0530

    api

--- university_app/settings.py
+++ university_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 import os 
 from pathlib import Path
 
@@ -162,3 +163,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
