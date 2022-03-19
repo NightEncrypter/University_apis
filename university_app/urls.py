78072a244c81a3b427a8480fff026ce0fd4d5d2c
MANAS <manasrathore2342@gmail.com>
2022-03-19 18:18:43 +0530

    migrated

--- university_app/urls.py
+++ university_app/urls.py
@@ -25,7 +25,11 @@ urlpatterns = [
     
 ]
 
-urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+# urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
 #     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #     from django.conf.urls.static import static
